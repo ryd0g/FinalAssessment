ba29b5bd6d88c1bd4f4df95f2be276b0ad425979
Ryan Lee <rjlguitar@gmail.com>
2020-10-06 12:14:42 -0700

    added font style input

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const display = document.querySelector('#display')
 //input variables
 const size = document.querySelector('#input-size')
 const font = document.querySelector('#select-font')
+const style = document.querySelector('#select-style')
 const color = document.querySelector('#input-color')
 const bg = document.querySelector('#input-bg-color')
 const text = document.querySelector('#enter-text')
 const showSize = document.querySelector('#show-size')
 const showFont = document.querySelector('#show-font')
+const showStyle = document.querySelector('#show-style')
 const showColor = document.querySelector('#show-color')
 const showBgColor = document.querySelector('#show-bg-color')
 
@@ -21,6 +23,10 @@ function handleInput() {
     const fontType = font.value
     display.style['font-family'] = fontType
     showFont.innerHTML = fontType
+
+    const fontStyle = style.value
+    display.style['font-style'] = fontStyle
+    showStyle.innerHTML = fontStyle
 
     const fontColor = color.value
     display.style['color'] = fontColor
@@ -38,6 +44,7 @@ function handleInput() {
 //event listeners
 size.addEventListener('input', handleInput)
 font.addEventListener('input', handleInput)
+style.addEventListener('input', handleInput)
 color.addEventListener('input', handleInput)
 bg.addEventListener('input', handleInput)
 text.addEventListener('input', handleInput)
